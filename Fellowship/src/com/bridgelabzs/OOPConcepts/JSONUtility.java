f38d59a6e9499ac9360d2d8f5a4612f33f68eba9
vaibhavkhadake <vaibhavkhadke@gmail.com>
2019-09-06 12:25:38 +0530

    Regex

--- Fellowship/src/com/bridgelabzs/OOPConcepts/JSONUtility.java
+++ Fellowship/src/com/bridgelabzs/OOPConcepts/JSONUtility.java
@@ -1,15 +1,19 @@
 package com.bridgelabzs.OOPConcepts;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JSONUtility
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws JSONException 
 	{
 		String file="/home/user/Desktop/Vaibhav_VK/Fellowship/InventoryDetails.json";
 		
 		JSONObject obj=new JSONObject();
-		obj.put("Rice", )
+		obj.put("RiceName", "Indrayani");
+		obj.put("Rice weight", 50);
+		obj.put("Rice price", 50);
+		
 		
 	}
 
